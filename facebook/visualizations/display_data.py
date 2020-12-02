5c7b34228c2ad27fe381a482646f13aeb7c71bf1
NLSanyu <lydiansanyu@gmail.com>
2020-12-02 14:39:01 +0300

    Process and display Facebook posts

--- facebook/visualizations/display_data.py
+++ facebook/visualizations/display_data.py
@@ -4,6 +4,7 @@ Display graphs of the data
 
 import streamlit as st
 import matplotlib.pyplot as plt
+import altair as alt
 from facebook.processing.process_data import process_posts
 
 
@@ -13,11 +14,12 @@ def display_facebook():
     """
     posts = process_posts()
     st.header("Facebook posts")
-    st.write(posts)
+    st.subheader("Likes")
     display_likes_graph(posts)
+    st.subheader("Total Interactions")
+    display_total_interactions_graph(posts)
 
 def display_likes_graph(posts):
-    st.subheader("Likes")
     fig, ax = plt.subplots()
     ax.plot(posts["date"], posts["statistics.actual.likeCount"])
     ax.set(title="Likes on MOH Facebook posts")
@@ -25,5 +27,8 @@ def display_likes_graph(posts):
     st.pyplot(fig)
 
 def display_total_interactions_graph(posts):
-    st.subheader("Total interactions")
-    st.line_chart(posts)
+    fig, ax = plt.subplots()
+    ax.plot(posts["date"], posts["total_interactions"])
+    ax.set(title="Total interactions on MOH Facebook posts")
+    ax.set(ylabel="Total interactions")
+    st.pyplot(fig)
