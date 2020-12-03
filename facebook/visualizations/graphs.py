99840f35d7066f97743497667b221f9dc2cf2add
NLSanyu <lydiansanyu@gmail.com>
2020-12-03 11:57:24 +0300

    Display highest performing posts

--- facebook/visualizations/graphs.py
+++ facebook/visualizations/graphs.py
@@ -4,7 +4,6 @@ Display graphs of the data
 
 import streamlit as st
 import matplotlib.pyplot as plt
-import altair as alt
 from facebook.processing.process_data import process_posts
 
 
@@ -12,11 +11,22 @@ def display_facebook():
     """
         Display Facebook posts data
     """
-    posts = process_posts()
-    st.subheader("Likes")
-    display_likes_graph(posts)
-    st.subheader("Total Interactions")
-    display_total_interactions_graph(posts)
+    posts, top_posts, summary = process_posts()
+    st.subheader(summary)
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        st.subheader("Total Interactions")
+        display_total_interactions_graph(posts)
+    with col2:
+        st.subheader("Likes")
+        display_likes_graph(posts)
+    with st.beta_expander("Highest perfoming posts"):
+        st.subheader("Highest performing posts (ranked by total interactions)")
+        post1, post2 = st.beta_columns(2)
+        post1.subheader("Highest ranking ")
+        post1.write(top_posts.at[0, "message"])
+        post2.write(top_posts.at[1, "message"])
+
 
 def display_likes_graph(posts):
     line_graph(
@@ -33,9 +43,8 @@ def display_total_interactions_graph(posts):
     )
 
 def line_graph(x, y, title, ylabel):
-    fig, ax = plt.subplots(figsize=(5,3))
+    fig, ax = plt.subplots()
     ax.plot(x, y)
     ax.set(title=title)
     ax.set(ylabel=ylabel)
     st.pyplot(fig)
-
