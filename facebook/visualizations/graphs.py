ab16097f357fbb41be319fc311f441899222ffef
NLSanyu <lydiansanyu@gmail.com>
2020-12-03 12:18:51 +0300

    Refactor displaying posts and summary

--- facebook/visualizations/graphs.py
+++ facebook/visualizations/graphs.py
@@ -11,22 +11,37 @@ def display_facebook():
     """
         Display Facebook posts data
     """
-    posts, top_posts, summary = process_posts()
-    st.subheader(summary)
-    col1, col2 = st.beta_columns(2)
-    with col1:
-        st.subheader("Total Interactions")
-        display_total_interactions_graph(posts)
-    with col2:
-        st.subheader("Likes")
-        display_likes_graph(posts)
+    posts, top_posts = process_posts()
+    summary(len(posts))
+    with st.beta_expander("Graphs of likes and total interactions"):
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            st.subheader("Total Interactions")
+            display_total_interactions_graph(posts)
+        with col2:
+            st.subheader("Likes")
+            display_likes_graph(posts)
     with st.beta_expander("Highest perfoming posts"):
         st.subheader("Highest performing posts (ranked by total interactions)")
         post1, post2 = st.beta_columns(2)
-        post1.subheader("Highest ranking ")
-        post1.write(top_posts.at[0, "message"])
-        post2.write(top_posts.at[1, "message"])
+        with post1:
+            display_post(top_posts, "Highest ranking post", 0)
+        with post2:
+            display_post(top_posts, "Second highest ranking post", 0)
 
+def summary(number_of_posts, accounts = None):
+    st.write(f"Number of posts: {number_of_posts}")
+    st.write(
+        """
+        Accounts
+        - Ministry of Health Uganda
+        """
+    )
+
+def display_post(top_posts, subheader, position):
+    st.subheader(subheader)
+    st.write(top_posts.at[position, "message"])
+    st.write(top_posts.at[position, "link"])
 
 def display_likes_graph(posts):
     line_graph(
