f6745d611ca5d46165aa21c548aed2edbfeac02a
NLSanyu <lydiansanyu@gmail.com>
2020-12-04 17:34:53 +0300

    Add docstrings to process_data functions

--- facebook/processing/process_data.py
+++ facebook/processing/process_data.py
@@ -2,7 +2,16 @@ from facebook.data.fetch_from_api import get_fb_posts
 
 
 def process_posts(metric="likes"):
-    posts = get_fb_posts(get_from_csv=True) # fetch posts from dummy file for now
+    """
+        Call the function that fetches posts from the API
+        and do some preliminary processing on them
+    """
+    # fetch posts from dummy file when testing
+    # posts = get_fb_posts(get_from_csv=True) 
+
+    # fetch posts from API
+    posts = get_fb_posts()
+
     posts.sort_values(by='date', inplace=True)
     posts['date'] = posts['date'].astype('datetime64')
     posts['date'] = posts['date'].dt.strftime('%b %d')
@@ -12,6 +21,10 @@ def process_posts(metric="likes"):
     return posts, top_posts
 
 def calculate_total_interactions(posts):
+    """
+        Calculate total interactions on posts by adding
+        up all interactions
+    """
     posts['total_interactions'] = posts['statistics.actual.likeCount'] 
     + posts['statistics.actual.shareCount']
     + posts['statistics.actual.commentCount']
@@ -26,6 +39,10 @@ def calculate_total_interactions(posts):
     return posts
 
 def highest_performing_posts(posts):
+    """
+        Pick the two posts with the highest 
+        total interactions
+    """
     top_posts = posts.sort_values(by='total_interactions', ascending=False)
     top_posts.reset_index(drop=True,inplace=True)
     return top_posts.head(2)
