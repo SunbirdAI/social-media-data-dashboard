76b52bbfbfc306877ce4cb8ac756814852ede483
NLSanyu <lydiansanyu@gmail.com>
2020-12-07 11:48:34 +0300

    Update info shown on top posts

--- facebook/visualizations/graphs.py
+++ facebook/visualizations/graphs.py
@@ -21,13 +21,13 @@ def display_facebook(start_date, end_date, mode):
         with col2:
             st.subheader("Likes")
             display_likes_graph(posts)
-    with st.beta_expander("Highest perfoming posts"):
-        st.header("Highest performing posts (ranked by total interactions)")
+    with st.beta_expander("Top posts for this time period"):
+        st.markdown("*Note:* _Top posts are ranked by total interactions_")
         post1, post2 = st.beta_columns(2)
         with post1:
-            display_post(top_posts, "Highest ranking post", 0)
+            display_post(top_posts, "Top post 1", 0)
         with post2:
-            display_post(top_posts, "Second highest ranking post", 1)
+            display_post(top_posts, "Top post 2", 1)
 
 def summary(number_of_posts, accounts = None):
     st.write(f"Number of posts: {number_of_posts}")
@@ -38,23 +38,40 @@ def summary(number_of_posts, accounts = None):
         """
     )
 
-def display_post(top_posts, subheader, position):
-    st.subheader(subheader)
-    total_int = top_posts.at[position, "total_interactions"]
-    st.write(f"Total Interactions: {total_int}")
-    text = top_posts.at[position, "message"]
+def display_post(top_posts, header, position):
+    st.header(header)
+
     link = top_posts.at[position, "link"]
     display_link = f"[Link to post]({link})"
 
     st.markdown(display_link, unsafe_allow_html=True)
+
+    total_int = top_posts.at[position, "total_interactions"]
+    likes = top_posts.at[position, "statistics.actual.likeCount"]
+    date = top_posts.at[position, "date"]
+
+    st.markdown(
+        f"""<div style='margin: 1 rem; padding: 1rem;
+            border: 1px solid #eee; border-radius: 1%;'>
+                <div><strong>Total interactions</strong>: {total_int}</div>
+                <div><strong>Likes</strong>: {likes}</div>
+                <div><strong>Date</strong>: {date}</div>
+            </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+    text = top_posts.at[position, "message"]
+
     st.markdown(
         f"""<div style='margin: 1 rem; padding: 1rem; height: 400px; 
-                border: 1px solid #eee; border-radius: 2%;'>
+                border: 1px solid #eee; border-radius: 1%;'>
                 <div>{text}</div>
             </div>
         """,
         unsafe_allow_html=True
     )
+
 
 def display_likes_graph(posts):
     line_graph(
