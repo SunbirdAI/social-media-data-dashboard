d3353ea666c6cd8f4347b46f32dc47c076436a18
Isaac Owomugisha <archimedesisaac@gmail.com>
2020-12-10 14:22:45 +0300

    Add fetch from api function

--- twitter/data/api_utils.py
+++ twitter/data/api_utils.py
@@ -1,9 +1,10 @@
 import os
 
+from typing import List
 from dotenv import load_dotenv
 
 
-def load_env_vars():
+def load_env_vars() -> dict:
     """
     Get Twitter api URL and bearer token
     :return: a dict with the URL and token
@@ -15,7 +16,7 @@ def load_env_vars():
     }
 
 
-def create_query(users, include_retweets=False):
+def create_query(users: List[str], include_retweets=False):
     """
     Creates the query string for fetching tweets from a given list of users
     :param users: the list of users
@@ -25,18 +26,19 @@ def create_query(users, include_retweets=False):
     return " OR ".join(f"from:{user}" for user in users) + (" -is:retweet" if not include_retweets else "")
 
 
-def create_url_with_query(api_root, query):
+def create_url_with_query(api_root: str, query: str):
     """
     Combines the root url with the query string
     :param api_root: the root url of the twitter api
     :param query: the query string
     :return: the url to which the request will be made
     """
-    tweet_fields = "tweet.fields=id,author_id,text,created_at,public_metrics&user.fields=name,username"
+    tweet_fields = "tweet.fields=id,author_id,text,created_at,public_metrics&expansions=author_id" \
+                   "&user.fields=name,username"
     return api_root + f"tweets/search/recent?query={query}&{tweet_fields}"
 
 
-def create_headers(bearer_token):
+def create_headers(bearer_token: str):
     """
     Creates the header to be sent along with the request to the api
     :param bearer_token: the authentication token
