c1488cabd5527ed9e3e0840e1b728e29443892e5
Isaac Owomugisha <archimedesisaac@gmail.com>
2020-12-18 13:01:37 +0300

    Add queries for tweets by mode and date

---
+++ twitter/data_layer/database/tweet_queries.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from twitter.data_layer.database.models.tweet import Tweet
+
+from twitter.data_layer.database.base import Session, engine, Base
+
+# generate database schema
+Base.metadata.create_all(engine)
+
+# create a new session
+session = Session()
+
+
+def get_tweets_by_mode_and_date(mode='moh', from_date=datetime(2000, 1, 1), to_date=datetime(2100, 1, 1)):
+    return session.query(Tweet) \
+        .filter(Tweet.mode == mode and from_date < Tweet.created_time < to_date) \
+        .all()
