9458c817f385e9ce15079c2a551e58e9fd8329c1
Isaac Owomugisha <archimedesisaac@gmail.com>
2020-12-18 14:20:25 +0300

    Move TITLE_TO_MODE dictionary to constants for reuse between facebook and twitter

--- constants.py
+++ constants.py
@@ -5,3 +5,9 @@ from nltk.corpus import stopwords
 ROOT = Path(__file__).resolve().parent
 DATA_FOLDER = ROOT.joinpath("data")
 STOP_WORDS = set(stopwords.words('english') + list(punctuation) + ['AT_USER', 'URL'])
+TITLE_TO_MODE = {
+    'Ministry of Health': 'moh',
+    'KCCA': 'kcca',
+    'Influencers': 'influencers',
+    'Engagers': 'engagers'
+}
