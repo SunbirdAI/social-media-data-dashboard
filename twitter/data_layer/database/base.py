537205f978d442c50e3366f5a5d78774d50faeb6
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-01-04 16:24:53 +0300

    Remove unnecessary check for database existence: this is what was causing the failure to connect to the db

--- twitter/data_layer/database/base.py
+++ twitter/data_layer/database/base.py
@@ -3,7 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database
 
 from dotenv import load_dotenv
 
@@ -11,8 +10,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
-if not database_exists(engine.url):
-    create_database(engine.url)
 
 Session = sessionmaker(bind=engine)
 
