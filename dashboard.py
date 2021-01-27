c2132aba5becc41142569da8eeacdc362c22421b
NLSanyu <lydiansanyu@gmail.com>
2021-01-27 14:43:26 +0300

    Update mode constants and database url

--- dashboard.py
+++ dashboard.py
@@ -4,13 +4,13 @@
 
 import streamlit as st
 import datetime
+from constants import MODES
 from facebook.visualizations.graphs import display_facebook
 from twitter.visualizations.render_graphs import display_twitter
 
 
 FACEBOOK = 'Facebook'
 TWITTER = 'Twitter'
-MODES = ['Ministry of Health', 'KCCA', 'Influencers', 'Engagers']
 
 st.set_page_config(layout='wide')
 st.sidebar.title("SunbirdAI Social Media Data Analysis Dashboard")
@@ -29,11 +29,14 @@ st.markdown(f"<h1 style='text-align: center;'>{platform} Analysis for {mode}</h1
             unsafe_allow_html=True)
 st.write('*Select a time period for the analysis*')
 start, end = st.beta_columns(2)
-start_date = start.date_input("Start date", datetime.date(2021, 1, 1))
+start_date = start.date_input(
+    "Start date",
+    datetime.date.today() - datetime.timedelta(days=14)
+)
 end_date = end.date_input("End date", datetime.date.today())
 
 
 if platform == FACEBOOK:
     display_facebook(start_date, end_date, mode)
-elif platform == TWITTER:
-    display_twitter(mode, start_date, end_date)
+# elif platform == TWITTER:
+#     display_twitter(mode, start_date, end_date)
